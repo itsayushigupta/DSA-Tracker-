cbe12e16a8a1b942b5834afc2ba1992f38ccaabb
Ayushi Gupta <104684877+itsayushigupta@users.noreply.github.com>
2023-03-01 23:11:41 +0530

    Add files via upload

---
+++ Binary Trees/creation.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d) {
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+/*
+              1
+        3           5
+     7    11    17    null
+*/
+
+node* buildTree(node* root) {
+
+    cout<<"enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+
+    if(data==-1) return NULL;
+
+    cout<<"enter data for inserting in left of "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"enter data for inserting in right of "<<data<<endl;
+    root->right=buildTree(root->right);
+}
+
+int main() {
+    node* root=NULL;
+    //creating a tree
+    root=buildTree(root);
+}
